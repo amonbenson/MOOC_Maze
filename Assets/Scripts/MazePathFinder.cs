bad6cd66e399cf392fc24413f4a08317894190a3
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2021-06-20 13:58:37 +0200

    bugfixes and massive speed improvements in pathfinder

--- Assets/Scripts/MazePathFinder.cs
+++ Assets/Scripts/MazePathFinder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Linq;
+using FibonacciHeap;
 
 public class MazePathFinder {
     private static readonly (Vector2Int, Maze.WallState)[] directions = {
@@ -17,87 +18,85 @@ public class MazePathFinder {
 
     public Stack<Vector2Int> AStar(Maze maze, Vector2Int origin, Vector2Int target) {
         this.maze = maze;
-        bounds = new BoundsInt(0, 0, 0, maze.size.x - 1, maze.size.y - 1, 1);
+        bounds = new BoundsInt(0, 0, 0, maze.size.x, maze.size.y, 1);
 
         // validation methods
         if (!bounds.Contains((Vector3Int) origin)) return null;
         if (!bounds.Contains((Vector3Int) target)) return null;
 
-        List<Node> openList = new List<Node>();
-        List<Node> closedList = new List<Node>();
-        List<Node> adjacencies;
+        FibonacciHeap<Node, int> openList = new FibonacciHeap<Node, int>(0);
+        Dictionary<Vector2Int, Node> closedList = new Dictionary<Vector2Int, Node>();
 
-        Node current = new Node(origin);
-        openList.Add(current);
+        Node currentNode = new Node(origin, null, 0, ComputeCost(origin, target));
+        openList.Insert(new FibonacciHeapNode<Node, int>(currentNode, currentNode.estimatedScore));
+        closedList.Add(currentNode.position, currentNode);
 
-        int counter = 100;
-        while (openList.Count > 0 && !closedList.Exists(x => x.position == target) && counter-- > 0) {
-            current = openList[0];
-            openList.Remove(current);
-            closedList.Add(current);
+        while (!openList.IsEmpty()) {
+            currentNode = openList.RemoveMin().Data;
 
-            adjacencies = GetAdjacentNodes(current);
-            foreach (var n in adjacencies) {
-                if (closedList.Contains(n)) continue;
-                if (openList.Contains(n)) continue;
+            if (currentNode.position == target) break;
 
-                // use manhattan distance to target as heuristic and increment the cost
-                n.parent = current;
-                n.heuristic = Math.Abs(n.position.x - target.x) + Math.Abs(n.position.y - target.y);
-                n.cost = n.parent.cost + 1;
+            // check each direction
+            foreach ((var direction, var wall) in directions) {
+                Vector2Int nextPosition = currentNode.position + direction;
 
-                openList.Add(n);
-                openList = openList.OrderBy(node => node.F).ToList<Node>();
+                // ignore if we are off the board or if there is a wall
+                if (!bounds.Contains((Vector3Int) nextPosition)) continue;
+                if (maze.walls[currentNode.position.x, currentNode.position.y].HasFlag(wall)) continue;
+
+                // calculate the score for this route
+                int nextScore = currentNode.routeScore + 1;
+
+                // if the next node exists already, make sure that the nextScore is better than the previous one
+                // if the node does not exist, create a new one and add it to the closed list
+                Node nextNode;
+                if (closedList.TryGetValue(nextPosition, out nextNode)) {
+                    if (nextScore >= nextNode.routeScore) continue;
+                } else {
+                    nextNode = new Node(nextPosition);
+                    closedList.Add(nextPosition, nextNode);
+                }
+
+                // update the node's parameters
+                nextNode.parent = currentNode;
+                nextNode.routeScore = nextScore;
+                nextNode.estimatedScore = nextScore + ComputeCost(nextPosition, target);
+
+                // put the node into the open list
+                openList.Insert(new FibonacciHeapNode<Node, int>(nextNode, nextNode.estimatedScore));
             }
         }
 
-        if (!closedList.Exists(x => x.position == target)) return null;
-        if (current == null) return null;
+        // if the target wasn't reached, no possible path was found
+        if (currentNode.position != target) return null;
 
         // walk back to the origin
         Stack<Vector2Int> path = new Stack<Vector2Int>();
         do {
-            path.Push(current.position);
-            current = current.parent;
-        } while (current != null && current.position != origin);
+            path.Push(currentNode.position);
+            currentNode = currentNode.parent;
+        } while (currentNode != null && currentNode.position != origin);
 
         return path;
     }
 
-    private List<Node> GetAdjacentNodes(Node node) {
-        List<Node> adjacencies = new List<Node>();
-
-        foreach ((var direction, var wall) in directions) {
-            Vector2Int p = node.position + direction;
-
-            if (bounds.Contains((Vector3Int) p) && !maze.walls[node.position.x, node.position.y].HasFlag(wall)) {
-                adjacencies.Add(new Node(p));
-            }
-        }
-
-        return adjacencies;
+    private int ComputeCost(Vector2Int from, Vector2Int to) {
+        return Mathf.Abs(from.x - to.x) + Mathf.Abs(from.y - to.y); // use manhattan distance
     }
 
     private class Node {
         public Vector2Int position;
-
         public Node parent;
-        public float heuristic;
-        public float cost;
+        public int routeScore;
+        public int estimatedScore;
 
-        public float F {
-            get {
-                if (heuristic == -1 || cost == -1) return -1;
-                else return heuristic + cost;
-            }
-        }
+        public Node(Vector2Int position) : this(position, null, int.MaxValue, int.MaxValue) {}
 
-        public Node(Vector2Int position) {
+        public Node(Vector2Int position, Node parent, int routeScore, int estimatedScore) {
             this.position = position;
-
-            parent = null;
-            heuristic = -1;
-            cost = 1;
+            this.parent = parent;
+            this.routeScore = routeScore;
+            this.estimatedScore = estimatedScore;
         }
     }
 }
